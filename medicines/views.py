c5bf3ba4f13c8bb7890aa2a815537b70dbf7d0f5
sashang magar <sashangmagar20@gmail.com>
2026-04-18 22:32:38 +0545

    wroking on medicine view

--- medicines/views.py
+++ medicines/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter , OrderingFilter
+from medicines.models import Medicine
+from medicines.serializers import MedicineSerializer
+from rest_framework.permissions import IsAuthenticated
+from store.permissions import FullDjangoModelPermission, IsAdminOrReadOnly
+from store.pagination import DefaultPagination
 
-# Create your views here.
+class MedicineViewSet(ModelViewSet):
+    serializer_class = MedicineSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Medicine.objects.filter(manufacturer = self.request.user)
