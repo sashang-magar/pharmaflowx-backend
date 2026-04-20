c58d3cb9c2148fa5dbca29f5ee25eb96a1dbd685
sashang magar <sashangmagar20@gmail.com>
2026-04-20 23:20:49 +0545

    admin for medicine and batch

--- medicines/admin.py
+++ medicines/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Medicine , Batch
 
-# Register your models here.
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display  = ['brand_name' , 'description' , 'strength' , 'unit_type']
+
+@admin.register(Batch)
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ['manufacturer_date' , 'expiry_date' , 'initial_quantity' , 'current_quantity' , 'mrp']    
+
