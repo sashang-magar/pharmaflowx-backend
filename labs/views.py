4677c7b1aa8ee5a3a2fefdbcdfd62a5474e165eb
sashang magar <sashangmagar20@gmail.com>
2026-04-27 23:56:31 +0545

    working on view of labreport

--- labs/views.py
+++ labs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from labs.serializers import LabReportSerializer 
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsManufacturer, IsLab, IsRegulator
 
-# Create your views here.
+class LabReportView(ModelViewSet):
+    serializer_class = LabReportSerializer
+
+    def get_permissions(self):
+        if self.action in ['create' , 'update' , 'partial_update']:
+            return [IsAuthenticated() , IsLab()]
+        return [IsAuthenticated()]
+    
+    def get_queryset(self):
+        return super().get_queryset()
