795762a6a45b7b1bcf9e61f11d098691a422a180
sashang magar <sashangmagar20@gmail.com>
2026-05-18 23:30:57 +0545

    working on approval views

--- approvals/views.py
+++ approvals/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ApprovalSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from .models import Approval
 # Create your views here.
+class ApprovalView(ModelViewSet):
+    serializer_class = ApprovalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == 'REGULATOR':
+            return Approval.objects.filter(
+                regulator = user
+                ).select_related(
+                    'lab_report__batch__medicine',
+                    'lab_report__batch__manufacturer'
+                )
