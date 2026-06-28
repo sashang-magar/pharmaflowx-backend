6e1d9026d75afe8bb8df98a049b8a3cfc9dc9701
sashang magar <sashangmagar20@gmail.com>
2026-06-28 09:09:41 +0545

    connect with postgresql successfully

--- pharmaflowx/settings.py
+++ pharmaflowx/settings.py
@@ -84,13 +84,24 @@ WSGI_APPLICATION = 'pharmaflowx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#For Postgresql database connection
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'pharma_db',
+       'USER': 'pharma_user',
+       'PASSWORD': 'root',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
