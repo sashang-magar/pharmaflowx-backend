7dd8b18347b47f51131ee391e60801722e4be082
sashang magar <sashangmagar20@gmail.com>
2026-08-16 12:47:14 +0545

    polished labs app's admin

--- labs/admin.py
+++ labs/admin.py
@@ -3,5 +3,7 @@ from . models import LabReport
 
 @admin.register(LabReport)
 class LabReportAdmin(admin.ModelAdmin):
-    list_display = ['batch' ,'lab' , 'result','report_status' ]
-
+    list_display = ['batch' ,'lab' , 'result','report_status', 'tested_at', 'created_at' ]
+    list_filter = ['result', 'report_status']
+    search_fields = ['batch__batch_number', 'lab__user__organization']
+    ordering = ['-created_at']
